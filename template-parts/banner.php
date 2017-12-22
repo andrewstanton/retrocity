96ff26767f7d6a9e1e868de8fe0be2ae117a93f6
Andrew Stanton <andrew@toddprod.com>
2017-12-21 20:30:59 -0500

    Updated navigation

--- template-parts/banner.php
+++ template-parts/banner.php
@@ -5,20 +5,6 @@ if(has_post_thumbnail()):
     $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
     $thumb_url = $thumb_url_array[0];
 ?>
-<section class="content-banner" style="background-image: url('<?php echo $thumb_url; ?>');">
-
-    <?php if(has_excerpt()){ ?>
-    <div class="banner-grid">
-        <div class="banner-excerpt">
-
-            <div class="excerpt-back">
-                <?php echo get_the_excerpt(); ?>
-            </div>
-        
-        </div>
-    </div>
-<?php } ?>
-
-</section>
+<section class="content-banner" style="background-image: url('<?php echo $thumb_url; ?>');"></section>
 
 <?php endif; ?>
