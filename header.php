11e404858b8415b992bb7502e4f35faefa4423e1
Andrew Stanton <andrew@toddprod.com>
2018-03-03 20:53:32 -0500

    Updated header

--- header.php
+++ header.php
@@ -41,7 +41,7 @@
 			<div class="site-branding">
 
 				<div class="logo-container">
-					<img src="<?php echo get_template_directory_uri() . '/images/logo.png';?>" alt="<?php echo bloginfo('name');?>"/>
+					<img src="<?php echo get_template_directory_uri() . '/images/logo.jpg';?>" alt="<?php echo bloginfo('name');?>"/>
 				</div>
 				
 			</div><!-- .site-branding -->
