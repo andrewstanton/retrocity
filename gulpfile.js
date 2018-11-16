d0f448651b224776d8449e40abedf3be40c887c9
Andrew <andrew@toddprod.com>
2018-11-16 16:54:47 -0500

    Updated gulp and packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
-var gulp = require('gulp'),
-sass = require('gulp-sass'),
-cleanCSS = require('gulp-clean-css'),
-rename = require("gulp-rename"),
-plumber = require('gulp-plumber'),
-gutil = require('gulp-util'),
-notify = require('gulp-notify'),
-concat = require('gulp-concat'),
-uglify = require('gulp-uglify');
-
-//var connect = require('gulp-connect');
+const { src, dest, watch, parallel, series } = require('gulp');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const rename = require("gulp-rename");
+const plumber = require('gulp-plumber');
+const gutil = require('gulp-util');
+const notify = require('gulp-notify');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 
 var config = {
     sassDir: ['./sass/main.scss'],
@@ -16,29 +14,31 @@ var config = {
     sassWatch: './sass/**/*.scss'
 }
 
-//keeps gulp from crashing for scss errors & gives sass access to bootstrap
-gulp.task('sass', function() {
-    gulp.src(config.sassDir)
-        .pipe(plumber({ errorHandler: function(err) {
-            notify.onError({
-                title: "Gulp error in " + err.plugin,
-                message:  err.toString()
-            })(err);
-            // play a sound once
-            gutil.beep();
-        }})) 
-        .pipe(sass())
-        .pipe(gulp.dest(config.sassOutputDir))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(cleanCSS())    
-        .pipe(gulp.dest(config.sassOutputDir));
-});
+const sassTask = (done) => {
+    src(config.sassDir).pipe(plumber({ errorHandler: function(err) {
+        notify.onError({
+            title: "Gulp error in " + err.plugin,
+            message:  err.toString()
+        })(err);
+        // play a sound once
+        gutil.beep();
+    }})) 
+    .pipe(sass())
+    .pipe(
+        dest(config.sassOutputDir)
+    )
+    .pipe(rename({
+        suffix: '.min'
+    }))
+    .pipe(cleanCSS())    
+    .pipe(
+        dest(config.sassOutputDir)
+    );
+    done();
+};
 
-gulp.task('scripts', function(){
-    gulp.src(config.jsDir + '*.js')
-    .pipe(plumber({ errorHandler: function(err) {
+const scriptTask = () => {
+    src(`${config.jsDir}*.js`).pipe(plumber({ errorHandler: function(err) {
         notify.onError({
             title: "Gulp error in " + err.plugin,
             message:  err.toString()
@@ -47,17 +47,25 @@ gulp.task('scripts', function(){
         gutil.beep();
     }}))
     .pipe(concat('app.js'))
-    .pipe(gulp.dest( config.publicJSCompDir ))
+    .pipe(
+        dest( config.publicJSCompDir )
+    )
     .pipe(rename({
         suffix: '.min'
       }))
     .pipe(uglify())
-    .pipe(gulp.dest( config.publicJSCompDir ));
-});
+    .pipe(
+        dest( config.publicJSCompDir )
+    );
+};
 
-gulp.task('watch', function() {
-    gulp.watch(config.sassWatch, ['sass']);
-    //gulp.watch(config.jsDir + '*.js', ['scripts']);
-});
+const watchSASS = () => {
+    watch(config.sassWatch, sassTask);
+};
 
-gulp.task('default', ['sass', 'watch']);
+const watchTask = parallel( watchSASS );
+const defaultTasks = (done) => {
+    series(sassTask, watchTask)(done);
+}
+
+exports.default = defaultTasks;
